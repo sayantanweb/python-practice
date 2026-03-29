3b8d81a5ff0385d7ab8ce267290b58375e377b83
Sayantan Samanta <sayantansamanta250@gmail.com>
2026-03-29 15:40:22 +0530

    add lists iteams

---
+++ add_two_list_element.py
@@ -0,0 +1,17 @@
+# list1=[1,2,3]
+# list2=[4,5,6]
+
+# res_lst=[]
+# for i in range(0,len(list1)):
+#   res_lst.append(list1[i]+list2[i])
+# print(res_lst)
+
+
+list1=[1,2,3]
+list2=[6,8,9]
+list3=[4,9,10]
+
+res_lst=[]
+for i in range(0,len(list1)):
+  res_lst.append(list1[i]+list2[i]+list3[i])
+print(res_lst)
